61d18bf2a38ccd6b66ef904d6a66a6f144e8d130
Staluks <janchukssss@inbox.lv>
2022-12-06 16:16:48 +0200

    fixed classes

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -3,14 +3,14 @@ package lang.print.gaps.task2;
 public class advancedNamingConvention {
     private static final int adult_age = 18;
 
-    private int AGE;
+    private int age;
     private int phoneNumber;
 
     void CAllToFriend() {
-        //some code
+        int number = phoneNumber;
     }
 
-    void callByNumber(int Number) {
-        //some code
+    void callByNumber(int number) {
+        phoneNumber = number;
     }
 }
